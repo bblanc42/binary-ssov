ff1c83d462b16d3bff53cfa668dc4e494a649323
Benoit Blanc <benoitblanc42@gmail.com>
2022-04-08 08:42:44 -0400

    feat: add test functions

--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -2,11 +2,52 @@
 pragma solidity 0.8.10;
 
 import "ds-test/test.sol";
+import "../Contract.sol";
 
 contract ContractTest is DSTest {
     function setUp() public {}
 
-    function testExample() public {
-        assertTrue(true);
+    function testCreateBet() public {
+        assertTrue(false);
+    }
+
+    function testNonOwnerCannotSettleOngoingEpoch() public {
+        assertTrue(false);
+    }
+
+    function testCannotSettleOngoingEpoch() public {
+        assertTrue(false);
+    }
+
+    function testSettleEndedEpoch() public {
+        assertTrue(false);
+    }
+
+    function testCannotDepositAtEnd() public {
+        assertTrue(false);
+    }
+
+    function testDepositBull() public {
+        assertTrue(false);
+    }
+
+    function testDepositBear() public {
+        assertTrue(false);
+    }
+
+    function testWinnerWithdraw() public {
+        assertTrue(false);
+    }
+
+    function testLoserWithdraw() public {
+        assertTrue(false);
+    }
+
+    function testCannotWithdraw() public {
+        assertTrue(false);
+    }
+
+    function testContract() public {
+        assertTrue(false);
     }
 }
